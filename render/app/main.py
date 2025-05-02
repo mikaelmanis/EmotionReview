715e1f15d0f86472f40037a96cf699cffdfd6f1a
mikaelmanis <mms27@hi.is>
2025-05-02 17:58:40 +0000

    final api

--- render/app/main.py
+++ render/app/main.py
@@ -4,7 +4,7 @@ from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 import torch.nn.functional as F
 from fastapi.middleware.cors import CORSMiddleware
-from emotionMapping import emotionID, emotionSentiment
+from app.emotionMapping import emotionID, emotionSentiment
 
 app = FastAPI()
 
