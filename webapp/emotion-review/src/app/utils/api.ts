f97572e7bbad7c3eed7005742ee3baef706f7ddc
mikaelmanis <mms27@hi.is>
2025-05-02 18:13:35 +0000

    tengt bakenda við framenda

--- webapp/emotion-review/src/app/utils/api.ts
+++ webapp/emotion-review/src/app/utils/api.ts
@@ -1,5 +1,5 @@
 export async function predictEmotion(text: string) {
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("https://emotionmodelapi.onrender.com/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
