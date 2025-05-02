844843d28830e7f05af5a366975d3cd83610a4be
mikaelmanis <mms27@hi.is>
2025-05-02 19:17:03 +0000

    vonandi x2

--- render/app/main.py
+++ render/app/main.py
@@ -16,7 +16,7 @@ model.eval()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://emotion-review.vercel.app/"],
+    allow_origins=["https://emotion-review.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
